4f6c2fd710c80f71b80ec350d04842c8312a8cb6
shawn <shawncaceres10@gmail.com>
2021-12-05 08:06:33 -0600

    header

--- src/Header.js
+++ src/Header.js
@@ -4,14 +4,17 @@ const Header = () => {
       
       <nav className='navbar'>
         
-        <a href='/'>
+        
           <h1 className='name'>
             Shawn Caceres
           </h1>
-        </a>
+       
         
-        <ul>
-        <a href="/">About</a>
+        <ul className='portfolio-links'>
+          <a href="/">Projects</a>
+          <a href="/">Blog</a>
+          
+          
         </ul>
       
       </nav>
