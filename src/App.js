e19548740df9eb47020348710870a38bb09392dc
shawn <shawncaceres10@gmail.com>
2021-12-05 06:54:28 -0600

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      yo
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      
+      <nav className='navbar'>
+        
+        <a href='/'>
+          <h1 className='name'>
+            Shawn Caceres
+          </h1>
+        </a>
+        
+        <ul>
+        <a href="/">About</a>
+        </ul>
+      
+      </nav>
+    
+    </header>
+    );
+}
+ 
+export default Header;
