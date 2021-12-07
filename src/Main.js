babe93bf1383a0785c186cd555d746b717527a6f
shawn <shawncaceres10@gmail.com>
2021-12-07 07:38:54 -0600

    skills section

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,8 @@
-import logo from './pics/react.png'
+import htmlLogo from './pics/html.png'
+import cssLogo from './pics/css.png'
+import javascriptLogo from './pics/javascript.png'
+import reactLogo from './pics/react.png'
+import githubLogo from './pics/github.png'
 
 const Main = () => {
   return (  
@@ -30,15 +34,53 @@ const Main = () => {
         <p className='skills-content-title'>
           skills
         </p>
-        <p className='work-content-paragraphs'>
-          I have been a wok chef, a gym manager, and a barista, but my favorite role has been that of a full-time father. 
-        </p>
-        <p className='work-content-paragraphs'>
-          Currently I am seeking opportunities to be part of a team as a front-end developer.
-        </p> 
+        
+        <div className='skills-logo-container'>
+          
+          <div className='img-logo-container'>
+            <img className='html-logo' src={htmlLogo}/>
+            <p className='logo-title'>
+              HTML
+            </p>
+          </div>
+
+          <div className='img-logo-container'>
+            <img className='css-logo' src={cssLogo}/>
+            <p className='logo-title'>
+              CSS
+            </p>
+          </div>
+
+          <div className='img-logo-container'>
+            <img className='javascript-logo' src={javascriptLogo}/>
+            <p className='logo-title'>
+              JAVASCRIPT
+            </p>
+          </div>
+
+          <div className='img-logo-container'>
+            <img className='react-logo' src={reactLogo}/>
+            <p className='logo-title'>
+              REACT
+            </p>
+          </div>
+
+          <div className='img-logo-container'>
+            <img className='github-logo' src={githubLogo}/>
+            <p className='logo-title'>
+              GITHUB
+            </p>
+          </div>
+          
+          
+          
+          
+          
+
+        </div>
         
       </div>
-<img src={logo}/>
+      
 
       
     </main>
