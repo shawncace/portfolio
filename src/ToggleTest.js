952c927257dae23a91bf00ad6e530e1be8d302d4
shawn <shawncaceres10@gmail.com>
2021-12-11 06:59:02 -0600

    toggle test triangle working

--- src/ToggleTest.js
+++ src/ToggleTest.js
@@ -1,7 +1,17 @@
 const ToggleTest = () => {
   return (  
     <div className='toggle-test-container'>
-      yo
+      <input type="checkbox" id='switch' className='toggle'/>
+      <label htmlFor="switch" className='lbl-toggle'>
+        LEARN MORE
+      </label>
+
+      <div className="collapsible-content">
+        <p>yo</p>
+        <p>yo</p>
+        <p>yo</p>
+        <p>yo</p>
+      </div>
     </div>
   );
 }
