2aa6e7c67f07ca5b8bc9cea88de592abd86a2a6a
shawn <shawncaceres10@gmail.com>
2022-01-20 10:43:59 -0600

    Blog section updated with first blog post

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,6 @@
 import DisplayBlog from "../hooks/DisplayBlog";
 import SeeMore from "./SeeMore";
+import ZoolanderComputer from '../pics/zoolandercomputer.png'
 
 const Blog = () => {
   return ( 
@@ -9,11 +10,16 @@ const Blog = () => {
       </p>
 
       <DisplayBlog
-        
+        title={'A Little Bit About Me'}
+        src={
+          ZoolanderComputer}
+        url={'https://shawncace.hashnode.dev/a-little-about-me'}
+        alt='Two men dancing around a computer'
+        description='My very first blog entry! An introduction into who I am and how my journey into coding began.'
       />
 
       <SeeMore 
-        url='https://github.com/shawncace'
+        url='https://shawncace.hashnode.dev/'
         title='Posts'
       />
     </section>
--- src/hooks/DisplayBlog.js
+++ src/hooks/DisplayBlog.js
@@ -1,6 +1,28 @@
-const DisplayBlog = () => {
+const DisplayBlog = (
+  {
+    title,
+    url,
+    src,
+    alt,
+    description
+  }
+) => {
   return (  
     <>
+      <div className="project-container">
+          <p className='project-title'>
+            {title}
+          </p>
+          
+          
+          <div className="project-img-container">
+            <a href={url} rel='noopener noreferrer' target='_blank'>
+              <img className='project-img'src={src} alt={alt}/>
+            </a>
+          </div>
+
+          <p className='blog-description'>{description}</p>
+      </div>
     </>
   );
 }
